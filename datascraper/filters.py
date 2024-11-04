6b975982cce73f14f098fa086d6c2647e70cc4b7
ManuelTrajcev <manueltrajcev7@gmail.com>
2024-11-04 20:48:06 +0100

    10 year data scrapng for company

--- datascraper/filters.py
+++ datascraper/filters.py
@@ -20,11 +20,7 @@ def filter_1():
 
     return filtered_options
 
-def initialize_driver():
-    # Initialize the Selenium WebDriver (assuming Chrome here, adjust as needed)
-    driver = webdriver.Chrome()
-    driver.get("https://www.mse.mk/mk/stats/symbolhistory/KMB")
-    return driver
+
 
 def set_date_range(driver, start_date, end_date):
     # Locate the date input fields and set the dates
@@ -35,27 +31,6 @@ def set_date_range(driver, start_date, end_date):
     to_date.clear()
     to_date.send_keys(end_date.strftime('%d.%m.%Y'))
 
-
-def select_issuer_code(driver, code):
-    # Locate the issuer code dropdown and select the appropriate option
-    select = Select(driver.find_element(By.ID, 'Code'))
-    select.select_by_value(code)
-
-# def scroll_to_bottom(driver):
-#     scrollable_div = driver.find_element(By.ID, 'mCSB_1_container')
-#     last_top = 0  # Initialize the top position
-#
-#     while True:
-#         # Adjust the top property to scroll down
-#         new_top = last_top - 1000  # Adjust the value to scroll down by 10 pixels
-#         driver.execute_script("arguments[0].style.top = arguments[1] + 'px';", scrollable_div, new_top)
-#         time.sleep(1)  # Wait for new data to load
-#
-#         # Check if new data has loaded
-#         current_top = int(driver.execute_script("return parseInt(arguments[0].style.top, 10);", scrollable_div))
-#         if current_top <= -scrollable_div.size['height']:  # Break if reached the bottom
-#             break
-#         last_top = new_top
 
 
 def convert_price_format(price_str):
@@ -101,64 +76,10 @@ def fetch_data(driver):
 
     return data # none for all other entries except the first 11 entries
 
-def save_data_to_db(data, issuer):
-    conn = sqlite3.connect('../db.sqlite3')  # Adjust path as needed
-    cursor = conn.cursor()
 
-    # Insert data into the database
-    for entry in data:
-        # Make sure to include the issuer in the entry
-        cursor.execute('''INSERT INTO transactions 
-                              (issuer, date, last_transaction_price, max_price, min_price, average_price, percent_change, 
-                              quantity, turnover_in_best, total_turnover)
-                              VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
-                       (issuer,) + entry)  # Prepend issuer to entry
+def filter_2(companies):
+    for company in companies:
 
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-def fetch_data_last_10_years(driver, issuer_code):
-    end_date = datetime.today()
-    start_date = end_date - timedelta(days=365)
-
-    # Divide the last 10 years into 365-day intervals
-    for _ in range(2): # should be 10 not 2
-        set_date_range(driver, start_date, end_date)
-        select_issuer_code(driver, issuer_code)
-        data = fetch_data(driver)
-        # save_data_to_db(data, issuer_code) # uncomment this to save to db
-
-        # Process and save data as needed
-        # print(data)
-
-        # Move to the previous interval
-        end_date = start_date - timedelta(days=1)
-        start_date = end_date - timedelta(days=365)
-
-def filter_2(data):
-    conn = sqlite3.connect('../db.sqlite3')
-    cursor = conn.cursor()
-
-    driver = initialize_driver()
-
-    issuer_last_dates = {}
-
-    for issuer in data[:2]: # remove [:2] only for testing
-        cursor.execute("SELECT MAX(date) FROM transactions WHERE issuer = ?", (issuer,))
-        result = cursor.fetchone()
-
-        if result[0] is not None:
-            last_date = result[0]
-            issuer_last_dates[issuer] = last_date
-        else:
-            print(f"Issuer: {issuer} does not exist in the database. Fetching data for the last 10 years...")
-            fetch_data_last_10_years(driver, issuer)
-
-    cursor.close()
-    conn.close()
-
-    return issuer_last_dates
 
 def filter_3(last_dates):
     conn = sqlite3.connect('../db.sqlite3')  # Adjust path as needed
@@ -276,11 +197,5 @@ def check_table_population():
     conn.close()
 
 if __name__ == '__main__':
-    # create_table();
-    # drop_transactions_table()
-    # check_table_exists('transactions')
-    issuers = filter_1()
-    last_dates = filter_2(issuers)
-    # filter_3(last_dates)
-    # check_table_population()
-    # print(results)
+    companies = filter_1()
+    filter_2(companies)
