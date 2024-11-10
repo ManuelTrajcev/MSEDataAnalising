efa8fc1774ea509c3f46a24ddbb294e993659622
ManuelTrajcev <manueltrajcev7@gmail.com>
2024-11-10 19:25:34 +0100

    domasna 1 - completed

--- DOMASNA_1/pipeline_list.py
+++ DOMASNA_1/pipeline_list.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from filters_list import filter_1, filter_2, filter_3
 
 def process_data(input_data):
@@ -6,7 +7,16 @@ def process_data(input_data):
     data = filter_2(data)
     print(data)
     data = filter_3(data)
-
+    return data
 
 if __name__ == "__main__":
-    process_data("https://www.mse.mk/mk/stats/symbolhistory/KMB")
+    data = process_data("https://www.mse.mk/mk/stats/symbolhistory/KMB")
+
+    print(f"Total number of new companies: {len(data)}")
+    total_data = 0
+    for company_code, data_per_company in data:
+        total_data += len(data_per_company)
+        df = pd.DataFrame(data_per_company)
+        df.to_csv(f'all_data/{company_code}.csv', index=False)
+
+    print(f"Total number of new data scraped: {total_data} (rows)")
--- DOMASNA_1/utils_lists.py
+++ DOMASNA_1/utils_lists.py
@@ -1,23 +1,13 @@
 import os
-import django
-import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 from bs4 import BeautifulSoup
 import requests
 import time
 from datetime import datetime, timedelta
-from selenium.webdriver.chrome.options import Options
 import pandas as pd
-
 
 def get_10_year_data_list(company_code):
     data = []
-    print("Thread started...")
-    start_time = time.time()
 
     years = 10
     end_date = datetime.now()
@@ -28,14 +18,9 @@ def get_10_year_data_list(company_code):
         end_date = start_date - timedelta(days=1)
         start_date = end_date - timedelta(days=365)
 
-    # driver.quit()
-
-    end_time = time.time()
-    execution_time = end_time - start_time
-    print("Thread finnished in {} seconds".format(execution_time))
-    df = pd.DataFrame(data)
-    df.to_csv(f'all_data/{company_code}.csv', index=False)
-
+    # df = pd.DataFrame(data)
+    # df.to_csv(f'all_data/{company_code}.csv', index=False)
+    return company_code, data
 
 def get_missing_data_list(company_code, date):
     data = []
@@ -63,7 +48,6 @@ def get_missing_data_list(company_code, date):
     old_data = pd.read_csv(f'all_data/{company_code}.csv')
     df = pd.concat([df, old_data], ignore_index=True)
     df.to_csv(f'all_data/{company_code}.csv', index=False)
-
 
 def search_company_year_list(company_code, to_date, from_date):
     data = []
@@ -105,7 +89,6 @@ def search_company_year_list(company_code, to_date, from_date):
                 data.append(columns_dict)
     return data
 
-
 def get_last_date(company_code):
     file_path = f'all_data/{company_code}.csv'
 
@@ -116,7 +99,6 @@ def get_last_date(company_code):
         return company_code, df.iloc[0]["date"]
     else:
         return company_code, None
-
 
 if __name__ == "__main__":
     start_time = time.time()
