365e6b4815b6b32be6b211c9e016157209e46243
ManuelTrajcev <manueltrajcev7@gmail.com>
2024-11-23 20:30:15 +0100

    React

--- MSEDataAnalising/settings.py
+++ MSEDataAnalising/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y0yuzayslcs2m*f($s9gx!xxxche7k60yhx0fp!q7ww7-!e_2q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'MSEDataAnalising.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [
+                 os.path.join(BASE_DIR, 'reactapp/build')
+                 ]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -123,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'reactapp/build/static')
+]
