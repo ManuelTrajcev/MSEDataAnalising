35987ffb101ad8c1fde35a825d867703c7574389
cakonchevat <139081286+cakonchevat@users.noreply.github.com>
2024-12-11 12:12:30 +0100

    Exclude local temporary folders in .gitignore

--- MSEDataAnalising/settings.py
+++ MSEDataAnalising/settings.py
@@ -124,9 +124,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'reactapp/build/static')
+    os.path.join(BASE_DIR, 'reactapp/build'),
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # React app
+    "http://127.0.0.1:3000",
 ]
