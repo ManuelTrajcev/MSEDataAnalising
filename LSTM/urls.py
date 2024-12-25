f65613d4e4b61377206db5387103e1a96410e347
dpetrova123 <dragana.petrova03@gmail.com>
2024-12-25 16:02:16 +0100

    django controllers for lstm and technical analysis

--- LSTM/urls.py
+++ LSTM/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
-# from .views import my_function
+from .views import get_prediction_data, oscillator_signals, moving_average_signals
 
 urlpatterns = [
-
+    path('api/lstm-predictions/', get_prediction_data, name='lstm-predictions'),
+    path('api/oscillator_signals/', oscillator_signals, name='oscillator-signals'),
+    path('api/moving_average_signals/', moving_average_signals, name='moving_average_signals')
 ]
