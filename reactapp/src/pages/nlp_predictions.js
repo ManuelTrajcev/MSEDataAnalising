485e4446d3f69ca14a4181e3cb86ae6f1546ee0d
ManuelTrajcev <manueltrajcev7@gmail.com>
2024-12-26 21:45:02 +0100

    get_copmay_predictions controller update and react api call updated

--- reactapp/src/pages/nlp_predictions.js
+++ reactapp/src/pages/nlp_predictions.js
@@ -1,6 +1,6 @@
 import NLP from "../components/NLP"
 
-export default function Test() {
+export default function Nlp_predictions() {
     return (
         <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
             <h1>Predictions</h1>
