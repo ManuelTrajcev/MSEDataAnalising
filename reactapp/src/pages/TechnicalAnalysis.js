8bfa9d0b18e94798f8c5846516bee16625cc6118
cakonchevat <139081286+cakonchevat@users.noreply.github.com>
2024-12-29 10:31:04 +0100

    Finishing touches to About, Issuers and Visualise pages

--- reactapp/src/pages/TechnicalAnalysis.js
+++ reactapp/src/pages/TechnicalAnalysis.js
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
 import TechnicalAnalysisBackground from "../components/AnalysisPage/TechnicalAnalysisBackground";
-import TimeSeriesChart from "../components/VisualizationPage/TimeSeriesChart";
+import LSTMChart from "../components/AnalysisPage/LSTMChart";
+import './TechnicalAnalysis.css'
+import Footer from "../components/Footer";
 
 export default function TechnicalAnalysis() {
-
     const [companyCodes, setCompanyCodes] = useState([]);
     const [chartData, setChartData] = useState([]);
     const [selectedCompanyCode, setSelectedCompanyCode] = useState("");
     const [technicalAnalysisData, setTechnicalAnalysisData] = useState({});
     const [message, setMessage] = useState("Please select a company to view the technical analysis.");
-    const [sentimentData, setSentimentData] = useState('');
+    const [sentimentData, setSentimentData] = useState("");
+    const [selectedTimeframe, setSelectedTimeframe] = useState("1D");
 
     useEffect(() => {
         const fetchCompanyCodes = async () => {
@@ -38,7 +40,7 @@ export default function TechnicalAnalysis() {
                         const date = new Date(companyDatum.date_string.split('.').reverse().join('-'));
                         return {
                             timestamp: date.toISOString(),
-                            value: companyDatum.last_transaction_price
+                            value: companyDatum.last_transaction_price,
                         };
                     });
 
@@ -46,13 +48,13 @@ export default function TechnicalAnalysis() {
                         const predictionDate = new Date(predictionsData.prediction_dates[index]);
                         return {
                             timestamp: predictionDate.toISOString(),
-                            value: prediction
+                            value: prediction,
                         };
                     });
 
                     const combinedData = [
                         ...formattedCompanyData,
-                        ...formattedPredictionData
+                        ...formattedPredictionData,
                     ].sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
 
                     const lastYearDate = new Date();
@@ -73,29 +75,25 @@ export default function TechnicalAnalysis() {
                     );
                     const movingAvgData = await responseMovingAvg.json();
 
-
                     const nlp_response = await fetch(
                         `http://localhost:8000/nlp/api/get-company-predictions/`
                     );
-                    const nlp_data = await nlp_response.json()
-
-                    console.log(nlp_data);
+                    const nlp_data = await nlp_response.json();
 
                     const sentiment = nlp_data.find((item) => item.company_code === selectedCompanyCode);
 
-                   if (sentiment) {
+                    if (sentiment) {
                         setSentimentData({
                             sentiment: sentiment.max_sentiment,
-                            sentiment_value: sentiment.max_sentiment_value
+                            sentiment_value: sentiment.max_sentiment_value,
                         });
                     }
 
                     setChartData(filteredData);
-                    // Combine all data
                     setTechnicalAnalysisData({
                         predictions: predictionsData,
                         oscillators: oscillatorData,
-                        movingAverages: movingAvgData
+                        movingAverages: movingAvgData,
                     });
                     setMessage(""); // Clear the message
                 } catch (error) {
@@ -138,128 +136,158 @@ export default function TechnicalAnalysis() {
     );
 
     const renderOscillators = (oscillators) => {
-        return Object.keys(oscillators).map((timeframe) => {
-            const data = oscillators[timeframe].map((oscillator) => ({
-                RSI: oscillator.RSI,
-                Signal_RSI: oscillator.Signal_RSI,
-                Stochastic_K: oscillator.stoch_k,
-                Signal_Stochastic_K: oscillator.Signal_stoch_k,
-                CCI: oscillator.cci,
-                Signal_CCI: oscillator.Signal_cci,
-                MACD: oscillator.macd,
-                Signal_MACD: oscillator.Signal_macd,
-                ADX: oscillator.adx,
-                Signal_ADX: oscillator.Signal_adx,
-            }));
+        const timeframeData = oscillators[selectedTimeframe] || [];
+        const data = timeframeData.map((oscillator) => ({
+            RSI: oscillator.RSI,
+            Signal_RSI: oscillator.Signal_RSI,
+            Stochastic_K: oscillator.stoch_k,
+            Signal_Stochastic_K: oscillator.Signal_stoch_k,
+            CCI: oscillator.cci,
+            Signal_CCI: oscillator.Signal_cci,
+            MACD: oscillator.macd,
+            Signal_MACD: oscillator.Signal_macd,
+            ADX: oscillator.adx,
+            Signal_ADX: oscillator.Signal_adx,
+        }));
 
-            const headers = [
-                "RSI",
-                "Signal_RSI",
-                "Stochastic_K",
-                "Signal_Stochastic_K",
-                "CCI",
-                "Signal_CCI",
-                "MACD",
-                "Signal_MACD",
-                "ADX",
-                "Signal_ADX",
-            ];
+        const headers = [
+            "RSI",
+            "Signal_RSI",
+            "Stochastic_K",
+            "Signal_Stochastic_K",
+            "CCI",
+            "Signal_CCI",
+            "MACD",
+            "Signal_MACD",
+            "ADX",
+            "Signal_ADX",
+        ];
 
-            return (
-                <div key={timeframe}>
-                    <h3>{timeframe}</h3>
-                    {renderTable(data, headers)}
-                </div>
-            );
-        });
+        return renderTable(data, headers);
     };
 
     const renderMovingAverages = (movingAverages) => {
-        return Object.keys(movingAverages).map((timeframe) => {
-            const data = movingAverages[timeframe].map((ma) => ({
-                SMA_50: ma["SMA(50)"],
-                Signal_SMA_50: ma.Signal_SMA_50,
-                SMA_200: ma["SMA(200)"],
-                Signal_SMA_200: ma.Signal_SMA_200,
-                EMA_50: ma["EMA(50)"],
-                Signal_EMA_50: ma.Signal_EMA_50,
-                EMA_200: ma["EMA(200)"],
-                Signal_EMA_200: ma.Signal_EMA_200,
-                Ichimoku_Baseline: ma.Ichimoku_Baseline,
-                Signal_Ichimoku_Baseline: ma.Signal_Ichimoku_Baseline,
-            }));
+        const timeframeData = movingAverages[selectedTimeframe] || [];
+        const data = timeframeData.map((ma) => ({
+            SMA_50: ma["SMA(50)"],
+            Signal_SMA_50: ma['Signal_SMA(50)'],
+            SMA_200: ma["SMA(200)"],
+            Signal_SMA_200: ma['Signal_SMA(200)'],
+            EMA_50: ma["EMA(50)"],
+            Signal_EMA_50: ma['Signal_EMA(50)'],
+            EMA_200: ma["EMA(200)"],
+            Signal_EMA_200: ma['Signal_EMA(200)'],
+            Ichimoku_Baseline: ma.Ichimoku_Baseline,
+            Signal_Ichimoku_Baseline: ma.Signal_Ichimoku_Baseline,
+        }));
 
-            const headers = [
-                "SMA_50",
-                "Signal_SMA_50",
-                "SMA_200",
-                "Signal_SMA_200",
-                "EMA_50",
-                "Signal_EMA_50",
-                "EMA_200",
-                "Signal_EMA_200",
-                "Ichimoku_Baseline",
-                "Signal_Ichimoku_Baseline",
-            ];
+        const headers = [
+            "SMA_50",
+            "Signal_SMA_50",
+            "SMA_200",
+            "Signal_SMA_200",
+            "EMA_50",
+            "Signal_EMA_50",
+            "EMA_200",
+            "Signal_EMA_200",
+            "Ichimoku_Baseline",
+            "Signal_Ichimoku_Baseline",
+        ];
 
-            return (
-                <div key={timeframe}>
-                    <h3>{timeframe}</h3>
-                    {renderTable(data, headers)}
-                </div>
-            );
-        });
+        return renderTable(data, headers);
     };
 
-     return (
-         <div id="container">
-             <div id="front">
-                 <TechnicalAnalysisBackground/>
-                 <h1>Техничка анализа на податоци</h1>
-             </div>
-             <div>
-                 <select onChange={handleCompanySelect} value={selectedCompanyCode}>
-                     <option value="">Select Company</option>
-                     {companyCodes.map((company, index) => (
-                         <option key={index} value={company.code}>
-                             {company.name}
-                         </option>
-                     ))}
-                 </select>
-             </div>
+    return ( <div id="tech-container">
+            <TechnicalAnalysisBackground />
+            <div id="tech-front">
+                <h1>Техничка анализа на податоци</h1>
+            </div>
 
-             {/* Display message if no company is selected */}
-             {message && <p className="error-message">{message}</p>}
+            <div>
+                <select onChange={handleCompanySelect} value={selectedCompanyCode}>
+                    <option value="">Select Company</option>
+                    {companyCodes.map((company, index) => (
+                        <option key={index} value={company.code}>
+                            {company.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
 
-             {/* Display technical analysis data */}
-             {technicalAnalysisData.predictions && (
-                 <TimeSeriesChart chartData={chartData} highlightLast10={true}/>
-             )}
+            {/* Display message if no company is selected */}
+            {!selectedCompanyCode && <p className="error-message">{message}</p>}
 
-             {/* Display Oscillators */}
-             {technicalAnalysisData.oscillators && (
-                 <div>
-                     <h2>Oscillator Signals</h2>
-                     {renderOscillators(technicalAnalysisData.oscillators)}
-                 </div>
-             )}
+            {/* Render only after a company is selected */}
+            {selectedCompanyCode && (
+                <>
+                    <div className="timeframe-buttons">
+                        <h3>Select Timeframe for Oscillators and Moving Averages</h3>
+                        <div className="button-container">
+                            {["1D", "1W", "1ME"].map((timeframe) => (
+                                <button
+                                    key={timeframe}
+                                    className={timeframe === selectedTimeframe ? "active" : ""}
+                                    onClick={() => setSelectedTimeframe(timeframe)}
+                                >
+                                    {timeframe}
+                                </button>
+                            ))}
+                        </div>
+                    </div>
 
-             {/* Display Moving Averages */}
-             {technicalAnalysisData.movingAverages && (
-                 <div>
-                     <h2>Moving Average Signals</h2>
-                     {renderMovingAverages(technicalAnalysisData.movingAverages)}
-                 </div>
-             )}
+                    {/* Display Oscillators */}
+                    {technicalAnalysisData.oscillators && (
+                        <div>
+                            <h2>Oscillator Signals</h2>
+                            {renderOscillators(technicalAnalysisData.oscillators)}
+                        </div>
+                    )}
 
-             {sentimentData ? (
-                 <div className="sentiment-data">
-                     <p><strong>Sentiment: </strong>{sentimentData.sentiment}</p>
-                     <p><strong>Sentiment Value: </strong>{sentimentData.sentiment_value}</p>
-                 </div>
-             ) : (
-                 <p>No sentiment data available</p>
-             )}
-         </div>
-     )
+                    {/* Display Moving Averages */}
+                    {technicalAnalysisData.movingAverages && (
+                        <div>
+                            <h2>Moving Average Signals</h2>
+                            {renderMovingAverages(technicalAnalysisData.movingAverages)}
+                        </div>
+                    )}
+
+                    {/* Display technical analysis data */}
+                    {technicalAnalysisData.predictions && (
+                        <div className="chart-container">
+                            <h2>LSTM Predictions</h2>
+                            <LSTMChart chartData={chartData} />
+                        </div>
+                    )}
+
+                    {/* Display Sentiment Data */}
+                    {sentimentData ? (
+                        <div className="sentiment-data">
+                            <h2>Sentiment Analysis</h2>
+                            <p>Sentiment:
+                                <strong className={sentimentData.sentiment === "positive" ? "sentiment-positive" :
+                                    sentimentData.sentiment === "negative" ? "sentiment-negative" : "sentiment-neutral"}>
+                                    {sentimentData.sentiment}
+                                </strong>
+                            </p>
+
+                            <p>Sentiment Value:
+                                <strong className={sentimentData.sentiment === "positive" ? "sentiment-positive" :
+                                    sentimentData.sentiment === "negative" ? "sentiment-negative" : "sentiment-neutral"}>
+                                    {sentimentData.sentiment_value.toFixed(2)}
+                                </strong>
+                            </p>
+
+                            {/* Sentiment Progress Bar */}
+                            <div className="progress-container">
+                                <div className={`progress-bar ${sentimentData.sentiment}`} style={{ width: `${sentimentData.sentiment_value * 100}%` }}></div>
+                            </div>
+                        </div>
+                    ) : (
+                        <p>No sentiment data available</p>
+                    )}
+                </>
+            )}
+            <Footer/>
+        </div>
+    );
 }
