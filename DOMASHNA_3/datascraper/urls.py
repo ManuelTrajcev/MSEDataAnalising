80ccb35eb0dd30c0a807b175354fff72057fd877
dpetrova123 <dragana.petrova03@gmail.com>
2024-12-29 23:23:29 +0100

    final

---
+++ DOMASHNA_3/datascraper/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('api/my-function/', my_function, name='my-function'),
+    path('api/get-data/', get_data, name='get-data'),
+    path('api/get-company-codes/', get_company_codes, name='get-company-codes'),
+    path('api/get-last-day-data/', get_last_day_data, name='get-last-day-data'),
+]
