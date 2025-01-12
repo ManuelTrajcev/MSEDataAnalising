240257c34ed476495dae51fce7147540d8a4747d
cakonchevat <139081286+cakonchevat@users.noreply.github.com>
2025-01-12 19:29:28 +0100

    Landing page css

--- reactapp/src/components/Navbar.js
+++ reactapp/src/components/Navbar.js
@@ -15,7 +15,7 @@ export default function Navbar() {
         <nav className="nav">
             <div className="logo">
                 <NavLink to="/">
-                    <img src={logo} alt="logo" width="280" height="66" />
+                    <img src={logo} alt="logo" width="260vh" height="60vh" />
                 </NavLink>
             </div>
             <div className="nav-links">
